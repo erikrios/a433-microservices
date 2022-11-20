22c4c59d9401a9ba3f1a5a4662d8543a5e22e0d7
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-11-20 19:15:51 +0700

    Create a shell script to build and push to Github container registry

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Create a Docker image from Dockerfile
+docker build -t karsajobs:latest .
+
+# Listing the local images
+docker images
+
+# Change the image name
+docker tag karsajobs:latest ghcr.io/erikrios/karsajobs:latest
+
+# Asking for Github username
+read -p 'Github username: ' username
+
+# Asking for  Github pesonal access token
+read -p 'Please input your Github personal access token: ' token
+
+echo "Authenticating Github credentials..."
+
+# Login into Github container registry
+echo $token | docker login ghcr.io -u $username --password-stdin
+
+# Push the image into Github container registry
+docker push ghcr.io/erikrios/karsajobs:latest
